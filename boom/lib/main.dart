174b84c5f034246f7e348ca5333db88640f50d0d
sakshyam adhikari <sakshyamadhikari>
2023-01-21 20:33:56 +0545

    on progress

---
+++ boom/lib/main.dart
@@ -0,0 +1,25 @@
+//import 'dart:html';
+//import 'package:';
+import 'package:flutter/material.dart';
+
+ void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatefulWidget {
+  const myApp({super.key});
+
+  @override
+  State <myApp> createState() =>  myAppState();
+}
+
+class  myAppState extends State <myApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text("mujiiiiiiiii randiiiii jathoooooo bccccccc jathiiiii hagneeeeeeee"),
+    ),
+    )
+  }
+}
